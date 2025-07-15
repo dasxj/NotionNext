b3d8a40f57c6cf7338c7e3dda6538925fef3653b
dasxj <lc@dasxj.com>
2025-07-15 11:58:07 +0800

    Update config.js

--- themes/hexo/config.js
+++ themes/hexo/config.js
@@ -20,7 +20,7 @@ const CONFIG = {
   HEXO_MENU_RANDOM: true, // 显示随机跳转按钮
 
   HEXO_POST_LIST_COVER: true, // 列表显示文章封面
-  HEXO_POST_LIST_COVER_HOVER_ENLARGE: false, // 列表鼠标悬停放大
+  HEXO_POST_LIST_COVER_HOVER_ENLARGE: true, // 列表鼠标悬停放大
 
   HEXO_POST_LIST_COVER_DEFAULT: true, // 封面为空时用站点背景做默认封面
   HEXO_POST_LIST_SUMMARY: true, // 文章摘要
@@ -35,7 +35,7 @@ const CONFIG = {
   HEXO_WIDGET_ANALYTICS: false, // 显示统计卡
   HEXO_WIDGET_TO_TOP: true,
   HEXO_WIDGET_TO_COMMENT: true, // 跳到评论区
-  HEXO_WIDGET_DARK_MODE: true, // 夜间模式
+  HEXO_WIDGET_DARK_MODE: false, // 夜间模式
   HEXO_WIDGET_TOC: true, // 移动端悬浮目录
 
   HEXO_THEME_COLOR: '#ebd61e' // 主题色配置（默认为 #928CEE）
