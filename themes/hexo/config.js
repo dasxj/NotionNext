191aa80f15640e1989821e4fb451704e770bdd70
dasxj <lc@dasxj.com>
2025-07-16 15:59:25 +0800

    Update config.js

--- themes/hexo/config.js
+++ themes/hexo/config.js
@@ -13,9 +13,9 @@ const CONFIG = {
 
   // 菜单配置
   HEXO_MENU_INDEX: true, // 显示首页
-  HEXO_MENU_CATEGORY: true, // 显示分类
-  HEXO_MENU_TAG: true, // 显示标签
-  HEXO_MENU_ARCHIVE: true, // 显示归档
+  HEXO_MENU_CATEGORY: false, // 显示分类
+  HEXO_MENU_TAG: false, // 显示标签
+  HEXO_MENU_ARCHIVE: false, // 显示归档
   HEXO_MENU_SEARCH: true, // 显示搜索
   HEXO_MENU_RANDOM: true, // 显示随机跳转按钮
 
@@ -34,7 +34,7 @@ const CONFIG = {
   HEXO_WIDGET_LATEST_POSTS: false, // 显示最新文章卡
   HEXO_WIDGET_ANALYTICS: false, // 显示统计卡
   HEXO_WIDGET_TO_TOP: true,
-  HEXO_WIDGET_TO_COMMENT: true, // 跳到评论区
+  HEXO_WIDGET_TO_COMMENT: false, // 跳到评论区
   HEXO_WIDGET_DARK_MODE: true, // 夜间模式
   HEXO_WIDGET_TOC: true, // 移动端悬浮目录
 
