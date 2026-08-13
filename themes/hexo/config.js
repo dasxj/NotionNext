22d459e9341dab08c83893ec72013352d552189b
dasxj <lc@dasxj.com>
2026-08-13 19:20:23 +0800

    Update config.js

--- themes/hexo/config.js
+++ themes/hexo/config.js
@@ -2,10 +2,7 @@ const CONFIG = {
   HEXO_HOME_BANNER_ENABLE: true,
   // 3.14.1以后的版本中，欢迎语在blog.config.js中配置，用英文逗号','隔开多个。
   HEXO_HOME_BANNER_GREETINGS: [
-    'Hi，我是一个程序员',
-    'Hi，我是一个打工人',
-    'Hi，我是一个干饭人',
-    '欢迎来到我的博客🎉'
+
   ], // 首页大图标语文字
 
   HEXO_HOME_NAV_BUTTONS: true, // 首页是否显示分类大图标按钮
@@ -16,11 +13,11 @@ const CONFIG = {
 
   // 菜单配置
   HEXO_MENU_INDEX: true, // 显示首页
-  HEXO_MENU_CATEGORY: true, // 显示分类
-  HEXO_MENU_TAG: true, // 显示标签
-  HEXO_MENU_ARCHIVE: true, // 显示归档
-  HEXO_MENU_SEARCH: true, // 显示搜索
-  HEXO_MENU_RANDOM: true, // 显示随机跳转按钮
+  HEXO_MENU_CATEGORY: false, // 显示分类
+  HEXO_MENU_TAG: false, // 显示标签
+  HEXO_MENU_ARCHIVE: false, // 显示归档
+  HEXO_MENU_SEARCH: false, // 显示搜索
+  HEXO_MENU_RANDOM: false, // 显示随机跳转按钮
 
   HEXO_POST_LIST_COVER: true, // 列表显示文章封面
   HEXO_POST_LIST_COVER_HOVER_ENLARGE: false, // 列表鼠标悬停放大
@@ -31,19 +28,19 @@ const CONFIG = {
   HEXO_POST_LIST_IMG_CROSSOVER: true, // 博客列表图片左右交错
 
   HEXO_ARTICLE_ADJACENT: true, // 显示上一篇下一篇文章推荐
-  HEXO_ARTICLE_COPYRIGHT: true, // 文章版权声明：true 全部显示；false 全部关闭；custom 仅填写 copyright 时显示
+  HEXO_ARTICLE_COPYRIGHT: false, // 文章版权声明：true 全部显示；false 全部关闭；custom 仅填写 copyright 时显示
   HEXO_ARTICLE_NOT_BY_AI: false, // 显示非AI写作
-  HEXO_ARTICLE_RECOMMEND: true, // 文章关联推荐
+  HEXO_ARTICLE_RECOMMEND: false, // 文章关联推荐
 
-  HEXO_WIDGET_LATEST_POSTS: true, // 显示最新文章卡
+  HEXO_WIDGET_LATEST_POSTS: false, // 显示最新文章卡
   HEXO_WIDGET_ANALYTICS: false, // 显示统计卡
   HEXO_WIDGET_TO_TOP: true,
-  HEXO_WIDGET_TO_COMMENT: true, // 跳到评论区
+  HEXO_WIDGET_TO_COMMENT: false, // 跳到评论区
   HEXO_WIDGET_DARK_MODE: true, // 夜间模式
   HEXO_WIDGET_TOC: true, // 移动端悬浮目录
 
-  HEXO_COLOR_PRIMARY: '#928CEE',
-  HEXO_THEME_COLOR: '#928CEE', // 主题色配置（默认为 #928CEE）
+  HEXO_COLOR_PRIMARY: '#ebd61e',
+  HEXO_THEME_COLOR: '#ebd61e', // 主题色配置（默认为 #928CEE）
 
   /** 文章详情页客户端切换时，主栏显示卡片+转圈占位（无全屏遮罩；已有独立 LoadingCover 的主题无需此项） */
   HEXO_ARTICLE_ROUTE_LOADING: true
